2a2a697f04263a3a76ef78e779519a9713b6448a
Will Walton <will@endless3dge.com>
2024-04-03 15:31:17 -0500

    Update MovieCard.js
    path added for fastar module

--- src/Components/Home/MovieCard/MovieCard.js
+++ src/Components/Home/MovieCard/MovieCard.js
@@ -3,7 +3,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { genres } from "../../../utils";
 
 import "./MovieCard.scss";
