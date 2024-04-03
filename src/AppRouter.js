c718dff089525c88c981a4d38c81ad5e938e18e6
Will Walton <will@endless3dge.com>
2024-04-03 13:34:42 -0500

    Update AppRouter.js
    router component declared in the AppRouter function
    
    props added in the header component tag
    switch component added
    
    path for the auth user components added to the function

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -9,3 +9,24 @@ import Home from "./views/Home/Home";
 import Header from "./containers/Header/Header";
 import Movie from "./views/NewMovies/Movie";
 
+const AppRouter = (props) => {
+    return (
+        <Router>
+            <Header {...props} />
+
+            <div className="m-container">
+                <Switch>
+                    <Route path="/login" exact component={Login}/>
+                    <Route path="/logout" exact component={Logout} />
+                    <Route path="/register" exact component={Register} />
+
+                    {/* users */}
+                    {/* movies */}
+                    <Route />
+                    {/* directors */}
+                    <Route />
+                </Switch>
+            </div>
+        </Router>
+    )
+}
