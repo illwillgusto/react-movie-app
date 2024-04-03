6b69e4785bab1c0fa8799faf7b4ac4cef366aa3c
Will Walton <will@endless3dge.com>
2024-04-03 14:58:03 -0500

    Update MovieFilter.js
    the movie filter component completed
    redux integration for mapStateToProps and mapDispatchToProps

--- src/Components/Home/MovieFilter/MovieFilter.js
+++ src/Components/Home/MovieFilter/MovieFilter.js
@@ -5,6 +5,7 @@ import { changeMovieFilterType } from "../../../store/movies/actions";
 import "../MovieFilter/MovieFilter.scss";
 
 import { filterTypes } from "../../../utils";
+import { connect } from "react-redux";
 
 const MovieFilter = (props) => {
     const filterButtons = filterTypes.map((btn) => {
@@ -19,7 +20,21 @@ const MovieFilter = (props) => {
              className={classes}
              key={btn.id}
              data-filter-type={btn.filterTypes}
-             onClick={(e) => props.changeMovieFilterType(e.target.dataset.filterType)}></button>
-        )
-    })
-}
+             onClick={(e) => props.changeMovieFilterType(e.target.dataset.filterType)}>
+                {btn.filterText}
+             </button>
+        );
+    });
+
+    return <div className="m-movieFilter">{filterButtons}</div>;
+};
+
+const mapStateToProps = (state) => ({
+    movies: state.movies,
+});
+
+const mapDispatchToProps = {
+    changeMovieFilterType,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieFilter);
