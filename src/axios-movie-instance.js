872a9459319e64e826032672c52051eec61ce521
Will Walton <will@endless3dge.com>
2024-04-03 15:50:53 -0500

    Update axios-movie-instance.js
    updated the import statement for Axios

--- src/axios-movie-instance.js
+++ src/axios-movie-instance.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import {Axios as axios} from "axios";
 
 let instance = axios.create({
    baseURL: "https://api.themoviedb.org/3/movie/550?api_key=e0b4355ea16e8f54e3163c98fa64f1c7" 
