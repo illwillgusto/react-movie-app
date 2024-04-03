8777bf4af6893c5c72a5265a3582c96fecc03047
Will Walton <will@endless3dge.com>
2024-04-03 13:26:12 -0500

    Create AppRouter.js
    created AppRouter component
    
    imported the Auth components
    imported the header, home, and movie components

---
+++ src/AppRouter.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Register from "./views/Auth/Register/Register";
+import Login from "./views/Auth/Login/Login";
+import Logout from "./views/Auth/Logout/Logout";
+
+import Home from "./views/Home/Home";
+import Header from "./containers/Header/Header";
+import Movie from "./views/NewMovies/Movie";
+
