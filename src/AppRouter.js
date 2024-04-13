932621254152a67df39d1f9f8f64d6d24dfa1227
Will Walton <will@endless3dge.com>
2024-04-13 18:55:48 -0500

    Update AppRouter.js
    rewrote the AppRouter code to fix current useRef null error occurring in the browser

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./views/Auth/Register/Register";
 import Login from "./views/Auth/Login/Login";
 import Logout from "./views/Auth/Logout/Logout";
-
 import Home from "./views/Home/Home";
 import Header from "./containers/Header/Header";
 import Movie from "./views/NewMovies/Movie";
@@ -16,15 +15,16 @@ const AppRouter = (props) => {
 
             <div className="m-container">
                 <Routes>
-                    {/* users */}
-                    <Route path="/login" exact component={Login}/>
-                    <Route path="/logout" exact component={Logout} />
-                    <Route path="/register" exact component={Register} />
+                    {/* Users */}
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/logout" element={<Logout />} />
+                    <Route path="/register" element={<Register />} />
 
-                    {/* movies */}
-                    <Route path="/movie/:movieId" exact component={Movie} />
-                    {/* directors */}
-                    <Route path="/" exact component={Home} />
+                    {/* Movies */}
+                    <Route path="/movie/:movieId" element={<Movie />} />
+
+                    {/* Home */}
+                    <Route path="/" element={<Home />} />
                 </Routes>
             </div>
         </Router>
@@ -32,3 +32,39 @@ const AppRouter = (props) => {
 };
 
 export default AppRouter;
+
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// import Register from "./views/Auth/Register/Register";
+// import Login from "./views/Auth/Login/Login";
+// import Logout from "./views/Auth/Logout/Logout";
+// import Home from "./views/Home/Home";
+// import Header from "./containers/Header/Header";
+// import Movie from "./views/NewMovies/Movie";
+
+// const AppRouter = (props) => {
+//     return (
+//         <Router>
+//             <Header {...props} />
+
+//             <div className="m-container">
+//                 <Routes>
+//                     {/* users */}
+//                     <Route path="/login" exact component={Login}/>
+//                     <Route path="/logout" exact component={Logout} />
+//                     <Route path="/register" exact component={Register} />
+
+//                     {/* movies */}
+//                     <Route path="/movie/:movieId" exact component={Movie} />
+//                     {/* directors */}
+//                     <Route path="/" exact component={Home} />
+//                 </Routes>
+//             </div>
+//         </Router>
+//     );
+// };
+
+// export default AppRouter;
