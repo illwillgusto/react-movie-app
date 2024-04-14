d75c8b429c5cd0255c5805a042c594ba95341466
Will Walton <will@endless3dge.com>
2024-04-13 20:19:05 -0500

    Update axios-movie-instance.js
    changed the baseURL to the popular page and have the api key included
    
    tested in Postman and the request worked as it should retrieving the necessary data

--- src/axios-movie-instance.js
+++ src/axios-movie-instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 let instance = axios.create({
-   baseURL: 'https://api.themoviedb.org/3' 
+   baseURL: 'https://api.themoviedb.org/3/movie/popular?api_key=e0b4355ea16e8f54e3163c98fa64f1c7' 
 });
 
 export default instance;
