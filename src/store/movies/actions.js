79e11545c8587d0fc5ed149371175f0d3005a817
Will Walton <will@endless3dge.com>
2024-04-14 08:13:13 -0500

    Update actions.js
    added the base url variable value

--- src/store/movies/actions.js
+++ src/store/movies/actions.js
@@ -6,7 +6,8 @@ import {
 
 import axios from "../../axios-movie-instance"
 
-const API_KEY = "e0b4355ea16e8f54e3163c98fa64f1c7"
+const API_KEY = "e0b4355ea16e8f54e3163c98fa64f1c7";
+const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const getMovies = (filterType = 'popular', currentPage = 1) => {
     return (dispatch) => {
