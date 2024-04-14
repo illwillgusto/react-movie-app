9fbcee90ea2661b5e1eb68190c2c456fa8b26195
Will Walton <will@endless3dge.com>
2024-04-14 08:33:23 -0500

    Update axios-instance.js
    changed the baseURL for axios-instance
    the localhost port is 3001 on my machine

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 let instance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://localhost:3001",
 });
 
 instance.defaults.headers.common["Authorization"] = localStorage.getItem(
