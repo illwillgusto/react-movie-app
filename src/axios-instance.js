f3f1a9890e9a741989fe0dec2f03646add3de885
Will Walton <will@endless3dge.com>
2024-04-14 08:24:10 -0500

    token update
    changed the authorization token to the API Read Access Token given from TMDB
    
    commented out the base url variable

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -5,7 +5,7 @@ let instance = axios.create({
 });
 
 instance.defaults.headers.common["Authorization"] = localStorage.getItem(
-    "wwToken"
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMGI0MzU1ZWExNmU4ZjU0ZTMxNjNjOThmYTY0ZjFjNyIsInN1YiI6IjY2MGFlODY1YTg4NTg3MDE3Y2U1ZmVkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1V18IXIWLUy3J11FA0oOp0fDAkE5gVsc4V5LLEu3fUM"
 );
 
 export default instance;
--- src/store/movies/actions.js
+++ src/store/movies/actions.js
@@ -7,7 +7,7 @@ import {
 import axios from "../../axios-movie-instance"
 
 const API_KEY = "e0b4355ea16e8f54e3163c98fa64f1c7";
-const BASE_URL = 'https://api.themoviedb.org/3';
+// const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const getMovies = (filterType = 'popular', currentPage = 1) => {
     return (dispatch) => {
