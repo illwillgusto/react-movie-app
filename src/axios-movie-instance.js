2712ecc8bc55a87b3e26aa5dd4cb52300f1d0bb5
Will Walton <will@endless3dge.com>
2024-04-14 09:13:15 -0500

    Update axios-movie-instance.js
    took code from the axios-instance and applied it
    
    this allows the app to now have authorization to the api_key request
    
    the popular movies are now appearing when the app is first loaded

--- src/axios-movie-instance.js
+++ src/axios-movie-instance.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
 
+const API_KEY = 'e0b4355ea16e8f54e3163c98fa64f1c7';
+
 let instance = axios.create({
-   baseURL: 'https://api.themoviedb.org/3/movie/popular?api_key=e0b4355ea16e8f54e3163c98fa64f1c7' 
+   baseURL: 'https://api.themoviedb.org/3'
 });
+
+instance.defaults.params = {};
+instance.defaults.params['api_key'] = API_KEY;
+
+
+// let instance = axios.create({
+//    baseURL: 'https://api.themoviedb.org/3/movie/popular?api_key=e0b4355ea16e8f54e3163c98fa64f1c7' 
+// });
 
 export default instance;
